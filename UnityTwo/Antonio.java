49ba7b02e8a2b933b7220f88cd5e764a9c59ed91
lazarok09 <lazarok09@gmail.com>
2021-01-20 19:20:14 -0300

    finished lesson 4

---
+++ UnityTwo/Antonio.java
@@ -0,0 +1,28 @@
+package UnityTwo;
+
+public class Antonio extends People {
+    public static void main(String[] args) {
+        People pessoa = new People();
+        System.out.println("-----First constructor with 4 params-----");
+        pessoa.People("Marco Antônio de Souza", 18, "homem", 1.80);
+        pessoa.status();
+        System.out.println("----Changing the class atributes with methods-------");
+        // commit - ading the getters and setters
+        /* changing with methods the atributes from "pessoa" object */
+
+        pessoa.setName("Marcos");
+        pessoa.setAge(28);
+        pessoa.setHeight(1.50f);
+        pessoa.setGender(null);
+        System.out.println(pessoa.getName());
+        System.out.println(pessoa.getAge());
+        System.out.println(pessoa.getGender());
+        System.out.println(pessoa.getHeight());
+
+        System.out.println("-----Constructor with 2 params------");
+        People maria = new People();
+        maria.People("mariana", 32);
+        maria.status();
+
+    }
+}
--- UnityTwo/People.java
+++ UnityTwo/People.java
@@ -1,28 +1,70 @@
 package UnityTwo;
 
 public class People {
-    private String name;
+    protected String name;
     private int age;
     private String gender;
     private double height;
+    private boolean authorization;
 
     // constructor meth
-    public void People(String name, int age, String gender, double height) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.height = height;
-    }
 
-    // constructor with out params
-    public People() {
-        // empety
+    public void People(String name, int age, String gender, double height) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+        setHeight(height);
     }
 
     // just two params;
-    public People(String name, int age) {
-        this.name = name;
-        this.age = age;
+    public void People(String name, int age) {
+        setAge(age);
+        setName(name);
+    }
+
+    // constructor with out params
+    public void People() {
+        // empety
+    }
+
+    public void status() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.gender);
+        System.out.println(this.height);
+    }
+
+    // methds getters and setters
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public String setName(String name) {
+        return this.name = name;
+    }
+
+    public String setGender(String gender) {
+        return this.gender = gender;
+    }
+
+    public double setHeight(double height) {
+        return this.height = height;
+    }
+
+    public int setAge(int age) {
+        return this.age = age;
     }
 
 }
